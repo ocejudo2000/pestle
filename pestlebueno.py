23e57e5fe7e2600f85164a822c72592a77388a4e
oscar cejudo <ocejudo@hotmail.com>
2023-10-12 11:25:23 -0600

    ultima version corregida para subir archivos excel

--- pestlebueno.py
+++ pestlebueno.py
@@ -13,7 +13,7 @@ def generar_resumen_pestle(nombre_empresa, caracteres_resumen):
     resumen_pestle = {}
 
     for dim in dimensiones_pestle:
-        prompt = f"Genera un resumen en formato bullet point sobre el aspecto {dim} de la empresa {nombre_empresa} en {caracteres_resumen} caracteres sin final cortado:"
+        prompt = f"Genera un resumen en formato bullet point sobre el aspecto {dim} de la empresa {nombre_empresa} en {caracteres_resumen} caracteres:"
         response = openai.Completion.create(
             engine="text-davinci-002",
             prompt=prompt,
@@ -21,15 +21,6 @@ def generar_resumen_pestle(nombre_empresa, caracteres_resumen):
         )
         resumen_pestle[dim] = response.choices[0].text.strip()
 
-    # Se añade la generación del resumen de competidores aquí, para que aparezca al final
-    prompt = f"Lista de principales competidores de la empresa {nombre_empresa}:"
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=50
-    )
-    resumen_pestle["Competidores"] = response.choices[0].text.strip()
-    
     return resumen_pestle
 
 # Interfaz Streamlit
@@ -41,24 +32,26 @@ if st.button("Generar Resumen PESTLE"):
     resumenes = generar_resumen_pestle(nombre_empresa, caracteres_resumen)
     for dim, resumen in resumenes.items():
         st.write(f"**{dim}**")
-        st.table({"Resumen": [resumen]})  # Aquí tu código para presentar el resumen en tablas
+        st.table({"Resumen": [resumen]})  # Presentación en tabla
 
-export_option = st.selectbox("¿Quieres exportar los resúmenes?", ["No", "Excel", "PowerPoint"])
+    export_option = st.selectbox("¿Quieres exportar los resúmenes?", ["No", "Excel", "PowerPoint"])
 
-if export_option == "Excel":
-    # Aquí tu código para permitir la descarga en Streamlit
-    df = pd.DataFrame(list(resumenes.items()), columns=["Aspect", "Summary"])
-    df.to_excel('resumenes_pestle.xlsx', index=False)
-    st.download_button("Descargar Excel", 'resumenes_pestle.xlsx', "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    if export_option == "Excel":
+        df = pd.DataFrame(list(resumenes.items()), columns=["Aspect", "Summary"])
+        df.to_excel('resumenes_pestle.xlsx', index=False)
+        st.download_button("Descargar Excel", 'resumenes_pestle.xlsx', "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
-elif export_option == "PowerPoint":
-    # Aquí tu código para permitir la descarga en Streamlit
-    prs = Presentation()
-    for dim, resumen in resumenes.items():
-        slide = prs.slides.add_slide(prs.slide_layouts[5])
-        title = slide.shapes.title
-        title.text = dim
-        content = slide.placeholders[1]
-        content.text = resumen
-    prs.save('resumenes_pestle.pptx')
-    st.download_button("Descargar PowerPoint", 'resumenes_pestle.pptx', "application/vnd.openxmlformats-officedocument.presentationml.presentation")
+    elif export_option == "PowerPoint":
+        # Generar PowerPoint
+        prs = Presentation()
+        for dim, resumen in resumenes.items():
+            slide_layout = prs.slide_layouts[1]
+            slide = prs.slides.add_slide(slide_layout)
+            title = slide.shapes.title
+            title.text = dim
+
+            content = slide.placeholders[1]
+            content.text = resumen
+
+        prs.save('resumenes_pestle.pptx')
+        st.download_button("Descargar PowerPoint", 'resumenes_pestle.pptx', "application/vnd.openxmlformats-officedocument.presentationml.presentation")
