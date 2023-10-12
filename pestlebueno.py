96cdd30a5b3e3ec254561fc51b05e86ca8ccbaf0
oscar cejudo <ocejudo@hotmail.com>
2023-10-12 10:31:07 -0600

    ultima version corregida para subir archivos excel

--- pestlebueno.py
+++ pestlebueno.py
@@ -48,14 +48,13 @@ elif export_option == "PowerPoint":
 
 import openai
 import streamlit as st
-import pandas as pd  # New: For Excel Export
-from pptx import Presentation  # New: For PowerPoint Export
+import pandas as pd
+from pptx import Presentation
 
-# Configure your OpenAI API key
+# Configura tu API key para OpenAI
 openai_api_key = st.secrets["mykey"]
 openai.api_key = openai_api_key
 
-# Function to generate PESTLE summary
 def generar_resumen_pestle(nombre_empresa, caracteres_resumen):
     dimensiones_pestle = ["Político", "Económico", "Sociocultural", "Tecnológico", "Legal", "Ecológico"]
     resumen_pestle = {}
@@ -69,7 +68,7 @@ def generar_resumen_pestle(nombre_empresa, caracteres_resumen):
         resumen_pestle[dim] = response.choices[0].text.strip()
     return resumen_pestle
 
-# Streamlit Interface
+# Interfaz Streamlit
 st.title("Análisis PESTLE")
 nombre_empresa = st.text_input("Ingresa el nombre de la Empresa:")
 caracteres_resumen = st.slider("Cantidad de caracteres para resumir por dimensión PESTLE:", 50, 500, 250)
@@ -79,19 +78,33 @@ if st.button("Generar Resumen PESTLE"):
     for dim, resumen in resumenes.items():
         st.write(f"**{dim}**")
         st.write(resumen)
-    
-    # New: Export to Excel
-    df = pd.DataFrame(list(resumenes.items()), columns=['Dimension', 'Resumen'])
-    with st.download_button("Descargar Excel", "resumenes_pestle.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"):
-        df.to_excel("resumenes_pestle.xlsx", index=False)
 
-    # New: Export to PowerPoint
+export_option = st.selectbox("¿Quieres exportar los resúmenes?", ["No", "Excel", "PowerPoint"])
+
+# ---------> HERE IS THE CHANGE <---------
+if export_option == "Excel":
+    # Create a Pandas DataFrame from the resumenes dict
+    df = pd.DataFrame(list(resumenes.items()), columns=["Aspect", "Summary"])
+    towrite = io.BytesIO()
+    downloaded_file = df.to_excel(towrite, encoding='utf-8', index=False, header=True)
+    towrite.seek(0)
+    if st.download_button('Download Excel', towrite, file_name='resumenes_pestle.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'):
+        pass
+
+elif export_option == "PowerPoint":
+    # Create a presentation object
     prs = Presentation()
     for dim, resumen in resumenes.items():
-        slide_layout = prs.slide_layouts[1]
-        slide = prs.slides.add_slide(slide_layout, dim, '')
-        content = slide.shapes.title
+        slide_layout = prs.slide_layouts[1] 
+        slide = prs.slides.add_slide(slide_layout)
+        title = slide.shapes.title
+        content = slide.placeholders[1]
+        title.text = dim
         content.text = resumen
-    prs.save("resumenes_pestle.pptx")
-    with st.download_button("Descargar PowerPoint", "resumenes_pestle.pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation"):
-        prs.save("resumenes_pestle.pptx")
+    
+    # Save to BytesIO object
+    ppt_io = io.BytesIO()
+    prs.save(ppt_io)
+    if st.download_button('Download PowerPoint', ppt_io, file_name='resumenes_pestle.pptx', mime='application/vnd.openxmlformats-officedocument.presentationml.presentation'):
+        pass
+
